ce63e99fd82fbffea8b09e39d328455e5e097c6f
Arun Kumar Sahoo <sahoo.kumar.arun@gmail.com>
2017-04-24 16:33:44 +0530

    reindented of RatingTestSuite
    Reindented to format codes into proper ordering.

--- client/test/controllers/ratingControllerSpec.js
+++ client/test/controllers/ratingControllerSpec.js
@@ -4,35 +4,35 @@ describe('VideoApp: RatingTestSuite', function() {
 	var $controller;
 
 	beforeEach(inject(function(_$controller_, _$cookies_, _$cookieStore_, _$httpBackend_, _$location_, _$rootScope_){
-		$controller  = _$controller_;
-		$cookies 	 = _$cookies_;
-		$httpBackend = _$httpBackend_;
-		$location 	 = _$location_;
-		$rootScope 	 = _$rootScope_;
-		$cookieStore = _$cookieStore_;
+		$controller 	= _$controller_;
+		$cookies 		= _$cookies_;
+		$httpBackend 	= _$httpBackend_;
+		$location 	 	= _$location_;
+		$rootScope 	 	= _$rootScope_;
+		$cookieStore 	= _$cookieStore_;
 	}));
 
 //Testing to update video rating by video id
-	it('should update video rating by video id', function() {
-	    var $scope = {};
-		var controller = $controller('ratingController', { $scope: $scope });
+it('should update video rating by video id', function() {
+	var $scope = {};
+	var controller = $controller('ratingController', { $scope: $scope });
 
-		$httpBackend.whenPOST("/video/ratings?sessionId="+$cookieStore.get('sessionId')).respond(200, {});
-	    $scope.rateFunction('597f075108ac282559d23b65', 4);
-	    
-	    $httpBackend.flush();
-	    expect($scope.valid).toBeTruthy();
-  	});
+	$httpBackend.whenPOST("/video/ratings?sessionId="+$cookieStore.get('sessionId')).respond(200, {});
+	$scope.rateFunction('597f075108ac282559d23b65', 4);
+
+	$httpBackend.flush();
+	expect($scope.valid).toBeTruthy();
+});
 
 //Testing video rating error message
-	it('should return error message', function() {
-	    var $scope = {};
-		var controller = $controller('ratingController', { $scope: $scope });
+it('should return error message', function() {
+	var $scope = {};
+	var controller = $controller('ratingController', { $scope: $scope });
 
-		$httpBackend.whenPOST("/video/ratings?sessionId="+$cookieStore.get('sessionId')).respond(404, {});
-	    $scope.rateFunction('597f075108ac282559d23b65', 7);
-	    
-	    $httpBackend.flush();
-	    expect($scope.valid).toBe(false);
-  	});
+	$httpBackend.whenPOST("/video/ratings?sessionId="+$cookieStore.get('sessionId')).respond(404, {});
+	$scope.rateFunction('597f075108ac282559d23b65', 7);
+
+	$httpBackend.flush();
+	expect($scope.valid).toBe(false);
+});
 });
